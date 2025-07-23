5b0e160d5d084fe5a59ea1a8dfbc5dcefbb95a0c
raushan-19 <tsubodh778@gmail.com>
2025-07-23 08:18:02 +0530

    javascript files

---
+++ Loops/01_first.js
@@ -0,0 +1,32 @@
+// print 1 to 5.
+
+// for(let i =1; i<=5; i++){
+//     console.log(i);
+// }
+
+// calculate sum of 1 to n.
+
+// let num = prompt("Enter your number : ");
+// let sum = 0;
+
+// for (let i = 1; i <= num; i++){
+//     sum = sum + i;
+// }
+
+// console.log("Sum = ",sum);
+ 
+// while loop 
+
+// let i= 1;
+// while (i<=5){
+//     console.log("i=",i);    // print 1 to 5
+//     i=i+1;
+// }
+
+// Do While loop
+
+let i = 1;
+do{
+    console.log(i);
+    i++;
+} while (i<= 5);
--- Operators/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-let a = 8;
-                // let b = 5;
-
-                // console.log("!(8<5) = ",!(a === 8));  //false
