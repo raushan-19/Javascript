669f7a5eb4969efe7bdd8e02e2805630bc2ae557
raushan-19 <tsubodh778@gmail.com>
2025-07-26 14:48:08 +0530

    js files

---
+++ PracticeQues/ques09.js
@@ -0,0 +1,9 @@
+// For a given array with prices of 5 items -> [250,645,300,900,50]. All items have an offer of 10% OFF on them. Change the array to store final price after applying offer.
+
+let items = [250,645,300,900,50];
+
+let idx = 0;
+for(let val of items){
+    console.log(`value at index ${idx} = ${val}`);
+    idx ++;
+}
