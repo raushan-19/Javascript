c8669f11ef8af8f834a3dbd4e1b4f6285b6d0f0f
raushan-19 <tsubodh778@gmail.com>
2025-07-29 08:07:24 +0530

    DOM Files

---
+++ DOM/script.js
@@ -0,0 +1,19 @@
+// let heading = document.getElementById("head1");     // way of getting id
+// console.log(heading);       // it will give the html part of "heading"
+// console.dir(heading)        // it will give the object 
+
+// let headings = document.getElementsByClassName("heading");
+// console.dir(headings);
+// console.log(headings)
+
+// let parahs = document.getElementsByTagName("p");
+// console.log(parahs);
+// console.dir(parahs);
+
+// let element = document.querySelector("p");      // returns first element
+// console.dir(element);
+
+let elementall = document.querySelectorAll("p");       //returns all elements
+console.dir(elementall);                      
+
+// NOTE :- in query selector we can pass "id", "class", "tag"
