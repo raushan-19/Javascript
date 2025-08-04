76c2b8924aa30cdab0e4a9b9ed994e5cda9abb40
raushan-19 <tsubodh778@gmail.com>
2025-08-04 18:56:26 +0530

    js files

--- DOM/script03.js
+++ DOM/script03.js
@@ -10,10 +10,22 @@
 // let para = document.querySelector("p");
 // console.log(para.setAttribute("class","newclass"));     // use to change Attribute name
 
-let div = document.querySelector("div");
-console.log(div);
+// let div = document.querySelector("div");
+// console.log(div);
 
-div.style.backgroundColor = "red";      // changing in style through JS
-div.style.fontSize = "25px";      
-div.innerText = "hello";
+// div.style.backgroundColor = "red";      // changing in style through JS
+// div.style.fontSize = "25px";      
+// div.innerText = "hello";
 // div.style.visibility = "hidden";
+
+// Insert Elements
+
+let newBtn = document.createElement("button");      // Creating new button
+newBtn.innerText = "click me!";                     // Writing something in the button 
+console.log(newBtn);                                // Showing the button on console
+
+let div = document.querySelector("div");            // Accessing the div
+// div.append(newBtn);                              // Adding button at the end of the div
+// div.prepend(newBtn);                                // Adding button at the starting of the div
+div.before(newBtn);                                // Adding button at the before of the div
+// div.after(newBtn);                                // Adding button at the after of the div
