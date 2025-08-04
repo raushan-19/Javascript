0f394ad9073c432fb27a88713d22e391eb4adde4
raushan-19 <tsubodh778@gmail.com>
2025-08-04 10:57:28 +0530

    js files

---
+++ DOM/script03.js
@@ -0,0 +1,11 @@
+let div = document.querySelector("div");
+console.log(div);
+
+let id = div.getAttribute("id");
+console.log(id);
+
+let name = div.getAttribute("name");
+console.log(name);
+
+let para = document.querySelector("p");
+console.log(para.setAttribute("class","newclass"));
