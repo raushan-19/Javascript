6bf4e0012821c520d799e54837ee5639f817b4cf
raushan-19 <tsubodh778@gmail.com>
2025-08-04 18:21:04 +0530

    js files

--- DOM/script03.js
+++ DOM/script03.js
@@ -1,11 +1,19 @@
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let name = div.getAttribute("name");   // use to get Attribute name
+// console.log(name);
+
+// let para = document.querySelector("p");
+// console.log(para.setAttribute("class","newclass"));     // use to change Attribute name
+
 let div = document.querySelector("div");
 console.log(div);
 
-let id = div.getAttribute("id");
-console.log(id);
-
-let name = div.getAttribute("name");
-console.log(name);
-
-let para = document.querySelector("p");
-console.log(para.setAttribute("class","newclass"));
+div.style.backgroundColor = "red";      // changing in style through JS
+div.style.fontSize = "25px";      
+div.innerText = "hello";
+// div.style.visibility = "hidden";
