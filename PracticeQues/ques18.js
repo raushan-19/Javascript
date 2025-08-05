44c5cd513eed1b09c610d5b7cb8416cf1d8d6e35
raushan-19 <tsubodh778@gmail.com>
2025-08-05 10:25:23 +0530

    js files

---
+++ PracticeQues/ques18.js
@@ -0,0 +1,6 @@
+// Create a new button element. Give it a text "click me", backgrpund color of red & text color of white.
+// Insert the button as the first Element inside the body tag.
+
+let btn = document.createElement("button");
+btn.innerText = "Click me!";
+console.log(btn);
