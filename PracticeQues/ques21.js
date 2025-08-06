b3c6b4fbf48c48c71e6fc7bb7d5d604e66099939
raushan-19 <tsubodh778@gmail.com>
2025-08-06 15:07:00 +0530

    practice question

--- PracticeQues/ques21.js
+++ PracticeQues/ques21.js
@@ -1,1 +1,22 @@
 // Create something on your website using "mouseover" event which affect your whole page.
+
+let box = document.querySelector(".box");
+let body = document.querySelector("body");
+let mode = "light";
+
+box.addEventListener("mouseover",(evt) => {
+    if(mode === "light"){
+
+        mode = "dark";
+        body.classList.remove("light");
+        body.classList.add("dark");
+    } else{
+
+        mode = "light";
+        body.classList.remove("dark");
+        body.classList.add("light");
+    }
+
+    console.log(mode);
+});
+
