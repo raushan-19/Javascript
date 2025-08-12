a81ceccb1246d67b35502f77528dd0cc1e038442
raushan-19 <tsubodh778@gmail.com>
2025-08-12 11:33:59 +0530

    practice ques

--- PracticeQues/ques22.js
+++ PracticeQues/ques22.js
@@ -1,8 +1,13 @@
 // Write a program using prompt function to take input of age as a value from the user and use alert to tell him if he can drive.
 
 let age = prompt("enter your age");
+age = Number.parseInt(age);  // it is use to convert age into a number 
 
-if(age >= 18){
+const canDrive = (age) =>{
+    return age >= 18 ? true : false;
+};
+
+if(canDrive(age)){
     alert("you can drive.");
 }else{
     alert("you can't drive!")
