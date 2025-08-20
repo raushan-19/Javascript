f550a4030e136a932ada048a78f79d314b0ead0b
raushan-19 <tsubodh778@gmail.com>
2025-08-20 13:56:41 +0530

    js files

--- fetch API/01_script.js
+++ fetch API/01_script.js
@@ -1,16 +1,16 @@
-let para = document.querySelector("#para");
-let btn = document.querySelector("#btn");
+let para = document.querySelector("#para");     // Accessing the paragraph 
+let btn = document.querySelector("#btn");   // Accessing the button
 
-let url = "https://catfact.ninja/facts";
+let url = "https://catfact.ninja/facts";    // Putting the url in a variable
 
-const getFacts = async () => {
-    console.log("Getting Data...");
-    let response = await fetch(url);
-    console.log(response);  // JSON format
-    let line = await response.json();
-    console.log(line.data);
-    let randomindex = Math.floor(Math.random() *  line.data.length);
-    para.innerText = line.data[randomindex].fact;
+const getFacts = async () => {     // making an async(arrow) function 
+    console.log("Getting Data...");     
+    let response = await fetch(url);    // fetching url with await in varaible "response"  
+    let line = await response.json();   // creating a variable "line" in which "response" is converted into JSON format
+    console.log(response); 
+    console.log(line.data); // This prints the array of "cat facts" that comes from the API  (data is array which stores all facts of this API)
+    let randomindex = Math.floor(Math.random() *  line.data.length);     // creating a random index between the length of the array "data"
+    para.innerText = line.data[randomindex].fact;    // it will print the value of a random fact which is extracted from the "data" array through "randomindex" 
 };
 
-btn.addEventListener("click", getFacts );
+btn.addEventListener("click", getFacts );  // on clicking the button "getFacts" will execute.
