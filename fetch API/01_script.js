f035c689c7a56654f3dff1005f6f1c7abe3a3d63
raushan-19 <tsubodh778@gmail.com>
2025-08-25 16:06:42 +0530

    API

--- fetch API/01_script.js
+++ fetch API/01_script.js
@@ -7,7 +7,7 @@ const getFacts = async () => {     // making an async(arrow) function
     console.log("Getting Data...");     
     let response = await fetch(url);    // fetching url with await in varaible "response"  
     let line = await response.json();   // creating a variable "line" in which "response" is converted into JSON format
-    console.log(response); 
+    console.log(line); 
     console.log(line.data); // This prints the array of "cat facts" that comes from the API  (data is array which stores all facts of this API)
     let randomindex = Math.floor(Math.random() *  line.data.length);     // creating a random index between the length of the array "data"
     para.innerText = line.data[randomindex].fact;    // it will print the value of a random fact which is extracted from the "data" array through "randomindex" 
